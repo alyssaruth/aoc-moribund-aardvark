2198800ec0a385f0fedc08f5392226b89e3d7aef
alyssa <alyssa.burlton@gmail.com>
2024-11-26 13:59:57 +0000

    1B

--- src/Solutions/Day1.hs
+++ src/Solutions/Day1.hs
@@ -1,14 +1,11 @@
 module Solutions.Day1
-  ( aoc1
-  ) where
+  where
 
 import           Common.AoCSolutions (AoCSolution (MkAoCSolution),
                                       printSolutions, printTestSolutions)
-import           Data.List           (tails)
 import           Data.Char           (isDigit)
 import           Text.Trifecta       (Parser, TokenParsing (token), alphaNum,
                                       some)
-import Common.ListUtils (window3, window2)
 
 aoc1 :: IO ()
 aoc1 = do
@@ -22,16 +19,35 @@ parseInput = do
   some $ token $ some alphaNum
 
 part1 :: CalibrationLines -> Int
-part1 x = sum (map calibrationValue x)
+part1 = sum . map calibrationValue
 
 part2 :: CalibrationLines -> Int
-part2 x = 10
+part2 x = sum ( map calibrationValue (map validNumbers x) )
 
 calibrationValue :: String -> Int
-calibrationValue x = read ([(firstNumber x)] ++ [(lastNumber x)])
+calibrationValue x = read [firstNumber x, lastNumber x]
 
 firstNumber :: String -> Char
-firstNumber x = (head ( filter isDigit x ) )
+firstNumber = head . filter isDigit
 
 lastNumber :: String -> Char
-lastNumber x = firstNumber ( reverse x )
+lastNumber = firstNumber . reverse
+
+validNumbers :: String -> String
+validNumbers [] = ""
+validNumbers l@(x:xs)
+    | isDigit x = [x] ++ validNumbers xs
+    | otherwise = (parseStringNum l) ++ (validNumbers xs)
+
+parseStringNum :: String -> String
+parseStringNum ('o':'n':'e':things) = ['1']
+parseStringNum ('t':'w':'o':stuff) = ['2']
+parseStringNum ('t':'h':'r':'e':'e':rubbish) = ['3']
+parseStringNum ('f':'o':'u':'r':remainder) = ['4']
+parseStringNum ('f':'i':'v':'e':gubbins) = ['5']
+parseStringNum ('s':'i':'x':doodad) = ['6']
+parseStringNum ('s':'e':'v':'e':'n':mcguffin) = ['7']
+parseStringNum ('e':'i':'g':'h':'t':junk) = ['8']
+parseStringNum ('n':'i':'n':'e':remnants) = ['9']
+parseStringNum x = []
+
