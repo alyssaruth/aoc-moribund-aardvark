0e2808ead65bd38c6816093a9e5882e9927be1fa
alyssa <alyssa.burlton@gmail.com>
2024-12-05 17:55:44 +0000

    Day 5

--- src/Solutions/Day5.hs
+++ src/Solutions/Day5.hs
@@ -1,21 +1,66 @@
 module Solutions.Day5
-  ( aoc5
-  ) where
+  ( aoc5,
+  )
+where
 
-import           Common.AoCSolutions (AoCSolution (MkAoCSolution),
-                                      printSolutions)
-import           Text.Trifecta       (Parser)
+import Common.AoCSolutions
+  ( AoCSolution (MkAoCSolution),
+    printSolutions,
+  )
+import Data.List
+import Data.Maybe
+import Text.Trifecta
+  ( Parser,
+    Parsing (eof, try),
+    char,
+    commaSep,
+    integer,
+    many,
+    manyTill,
+    sepBy,
+  )
 
 aoc5 :: IO ()
 aoc5 = do
   printSolutions 5 $ MkAoCSolution parseInput part1
   printSolutions 5 $ MkAoCSolution parseInput part2
 
-parseInput :: Parser String
-parseInput = undefined
+type Page = [Integer]
 
-part1 :: String -> String
-part1 = undefined
+type PageRule = (Integer, Integer)
 
-part2 :: String -> String
-part2 = undefined
+parseInput :: Parser ([PageRule], [Page])
+parseInput = do
+  pageRules <- many $ try parsePageRule
+  pages <- manyTill (commaSep integer) eof
+  pure (pageRules, pages)
+
+parsePageRule :: Parser (Integer, Integer)
+parsePageRule = do
+  [a, b] <- sepBy integer (char '|')
+  pure (a, b)
+
+part1 :: ([PageRule], [Page]) -> Integer
+part1 (rules, pages) = sum $ map middleElement $ filter (`elem` pages) sortedPages
+  where
+    sortedPages = map (sortBy (ruleSort rules)) pages
+
+ruleSort :: [PageRule] -> Integer -> Integer -> Ordering
+ruleSort rules x y = compare (tupleIndex x relevantRule) (tupleIndex y relevantRule)
+  where
+    relevantRule = fromMaybe (-1, -1) $ find (ruleContains x y) rules
+
+ruleContains :: Integer -> Integer -> PageRule -> Bool
+ruleContains x y rule = rule == (x, y) || rule == (y, x)
+
+tupleIndex :: Integer -> (Integer, Integer) -> Maybe Int
+tupleIndex x (a, b) = elemIndex x [a, b]
+
+middleElement :: Page -> Integer
+middleElement [x] = x
+middleElement xs = middleElement $ init $ tail xs
+
+part2 :: ([PageRule], [Page]) -> Integer
+part2 (rules, pages) = sum $ map middleElement $ filter (not . (`elem` pages)) sortedPages
+  where
+    sortedPages = map (sortBy (ruleSort rules)) pages
