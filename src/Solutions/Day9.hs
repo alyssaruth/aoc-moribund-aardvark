b70d27153b5de70f38316be8844aaeb8cc29b797
alyssa <alyssa.burlton@gmail.com>
2024-12-11 11:06:08 +0000

    working, disgusting, slow, day 9 part 1

--- src/Solutions/Day9.hs
+++ src/Solutions/Day9.hs
@@ -1,10 +1,16 @@
 module Solutions.Day9
-  ( aoc9
-  ) where
+where
 
-import           Common.AoCSolutions (AoCSolution (MkAoCSolution),
-                                      printSolutions)
-import           Text.Trifecta       (Parser)
+import Common.AoCSolutions
+  ( AoCSolution (MkAoCSolution),
+    printSolutions, printTestSolutions,
+  )
+import Data.Char
+import Text.Trifecta (CharParsing (anyChar), Parser, many)
+import Control.Lens
+import Data.List.Split
+import Data.Maybe
+import Data.Sequence (elemIndexR, fromList, Seq, mapWithIndex, elemIndexL, update, deleteAt, findIndexR, index)
 
 aoc9 :: IO ()
 aoc9 = do
@@ -12,10 +18,31 @@ aoc9 = do
   printSolutions 9 $ MkAoCSolution parseInput part2
 
 parseInput :: Parser String
-parseInput = undefined
+parseInput = many anyChar
 
-part1 :: String -> String
-part1 = undefined
+--part1 :: String -> Int
+--part1 :: String -> [Maybe Int]
+part1 = checksum . compress . parseMap
+
+parseMap :: String -> Seq (Maybe Int)
+parseMap = fromList . concat . imap parseMapElement . chunksOf 2 . map digitToInt
+
+parseMapElement :: Int -> [Int] -> [Maybe Int]
+parseMapElement index [file, free] = replicate file (Just index) ++ replicate free Nothing
+parseMapElement index [file] = replicate file (Just index)
+
+checksum :: Seq (Maybe Int) -> Int
+checksum = sum . mapWithIndex blockChecksum
+
+blockChecksum :: Int -> Maybe Int -> Int
+blockChecksum ix block = ix * fromMaybe 0 block
+
+compress :: Seq (Maybe Int) -> Seq (Maybe Int)
+compress x = if firstNothingIx == lastJustIx + 1 then x else compress $ update firstNothingIx lastJust $ update lastJustIx Nothing x
+  where
+    firstNothingIx = fromJust $ elemIndexL Nothing x
+    lastJustIx = fromJust $ findIndexR isJust x
+    lastJust = x `Data.Sequence.index` lastJustIx
 
 part2 :: String -> String
 part2 = undefined
