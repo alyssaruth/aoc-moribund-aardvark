788743b24bf145f0d3062e931bc7546e399af409
alyssa <alyssa.burlton@gmail.com>
2024-12-17 18:47:59 +0000

    WIP 15B, new implementation working for part 1

--- src/Solutions/Day15.hs
+++ src/Solutions/Day15.hs
@@ -1,7 +1,4 @@
-module Solutions.Day15
-  ( aoc15,
-  )
-where
+module Solutions.Day15 where
 
 import Common.AoCSolutions
   ( AoCSolution (MkAoCSolution),
@@ -9,9 +6,19 @@ import Common.AoCSolutions
     printTestSolutions,
   )
 import Common.Geometry
+import Control.Lens ((^.))
+import Data.List (intersect, minimumBy, (\\), singleton)
 import qualified Data.Map as M
-import Linear (V2 (..))
+import Data.Set (Set)
+import qualified Data.Set as S
+import Linear.V2 (R1 (_x), R2 (_y), V2 (..))
 import Text.Trifecta (CharParsing (anyChar, string), Parser, Parsing (try), TokenParsing (token), manyTill, oneOf, some)
+
+type Box = [Point]
+
+data Warehouse = Warehouse {robot :: Point, boxes :: [Box], walls :: Set Point}
+
+data MoveResult = MoveResult {affectedBoxes :: [Box], hitWall :: Bool}
 
 aoc15 :: IO ()
 aoc15 = do
@@ -44,6 +51,44 @@ makeMove g direction = if result == '#' then g else shiftAll g allPts
     allPts = collectBoxes g [robot] direction
     result = M.findWithDefault ' ' (last allPts) g
 
+factoryWarehouse :: Grid -> Warehouse
+factoryWarehouse g = Warehouse (findRobot g) (findBoxes g) (M.keysSet $ M.filter (== '#') g)
+
+scoreWarehouse :: Warehouse -> Int
+scoreWarehouse = sum . map boxGpsCoordinates . boxes
+
+boxGpsCoordinates :: Box -> Int
+boxGpsCoordinates box = x + 100 * y
+  where
+    x = minimum $ map (^. _x) box
+    y = minimum $ map (^. _y) box
+
+makeMoveNew :: Warehouse -> Point -> Warehouse
+makeMoveNew warehouse direction = updateWarehouse warehouse direction result
+  where
+    result = exploreMove warehouse direction [robot warehouse] []
+
+exploreMove :: Warehouse -> Point -> [Point] -> [Box] -> MoveResult
+exploreMove warehouse direction latestPoints currentBoxes
+  | hitWall = MoveResult [] True
+  | null newBoxes = MoveResult currentBoxes False
+  | otherwise = exploreMove warehouse direction (concat newBoxes) (currentBoxes ++ newBoxes)
+  where
+    newPoints = map (+ direction) latestPoints
+    hitWall = not $ null $ walls warehouse `S.intersection` S.fromList newPoints
+    newBoxes = [box | box <- boxes warehouse, not $ null $ box `intersect` newPoints]
+
+updateWarehouse :: Warehouse -> Point -> MoveResult -> Warehouse
+updateWarehouse (Warehouse robot boxes walls) direction (MoveResult affectedBoxes hitWall)
+  | hitWall = Warehouse robot boxes walls
+  | otherwise = Warehouse (robot + direction) (shiftedBoxes ++ unaffectedBoxes) walls
+  where
+    unaffectedBoxes = boxes \\ affectedBoxes
+    shiftedBoxes = map (shiftBox direction) affectedBoxes
+
+shiftBox :: Point -> Box -> Box
+shiftBox direction = map (+ direction)
+
 -- Replace guard with ., last space with O, then second space with @ (overwriting O if there were no boxes)
 shiftAll :: Grid -> [Point] -> Grid
 shiftAll g pts = M.insert (head (tail pts)) '@' $ M.insert (last pts) 'O' $ M.insert (head pts) '.' g
@@ -57,8 +102,13 @@ collectBoxes g pts direction = if item == 'O' then collectBoxes g (pts ++ [nextP
 findRobot :: Grid -> Point
 findRobot = head . M.keys . M.filter (== '@')
 
+findBoxes :: Grid -> [Box]
+findBoxes = map singleton . M.keys . M.filter (== 'O')
+
 part1 :: (Grid, [Point]) -> Int
 part1 (g, moves) = sumGpsCoordinates $ foldl makeMove g moves
 
-part2 :: (Grid, [Point]) -> String
-part2 = undefined
+part2 :: (Grid, [Point]) -> Int
+part2 (g, moves) = scoreWarehouse $ foldl makeMoveNew warehouse moves
+  where
+    warehouse = factoryWarehouse g
