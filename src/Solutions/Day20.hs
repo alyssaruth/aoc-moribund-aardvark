0066157fedbaa85565567976aa9d6c0a569bb329
alyssa <alyssa.burlton@gmail.com>
2024-12-26 22:54:51 +0000

    slightly faster & tidier 20

--- src/Solutions/Day20.hs
+++ src/Solutions/Day20.hs
@@ -1,23 +1,22 @@
-module Solutions.Day20
-where
+module Solutions.Day20 where
 
 import Common.AoCSolutions
   ( AoCSolution (MkAoCSolution),
-    printSolutions, printTestSolutions,
+    printSolutions,
+    printTestSolutions,
   )
 import Common.Geometry
-import Text.Trifecta (CharParsing (anyChar), Parser, many)
-import Data.Set (Set, elemAt, (\\), notMember, union, fromList, intersection, toList)
+import Common.ListUtils (associateBy)
+import Data.List (nub)
+import qualified Data.List
 import qualified Data.Map as M
 import Data.Maybe (mapMaybe)
-import qualified Data.List
-import Linear (V2(V2))
-import Data.List (nub)
+import Data.Set (Set, elemAt, (\\))
+import Linear (V2 (V2))
+import Text.Trifecta (CharParsing (anyChar), Parser, many)
 
-data RaceState = RaceState{visited :: M.Map Position Int, walls :: Set Position, end :: Position, time :: Int, position :: Position}
+data RaceState = RaceState {visited :: M.Map Position Int, walls :: Set Position, end :: Position, time :: Int, position :: Position}
   deriving (Show, Eq, Ord)
-
-data PartialCheat = PartialCheat{steps :: Int, points :: [Position]}
 
 aoc20 :: IO ()
 aoc20 = do
@@ -45,48 +44,43 @@ race grid = completeRace grid startingState
   where
     start = locate 'S' grid
     end = locate 'E' grid
-    walls = M.keysSet $ M.filter (=='#') grid
+    walls = M.keysSet $ M.filter (== '#') grid
     startingState = RaceState M.empty walls end 0 start
 
-attemptCheat :: Grid -> Int -> M.Map Position Int -> (Position, Int) -> [Int]
-attemptCheat grid maxDepth timeFromPosition (startPosition, time) = concatMap (cheatScores timeFromPosition time) partialCheats
-  where
-    distances = [2..maxDepth]
-    partialCheats = map (toPartialCheat startPosition) distances
+attemptCheat :: Int -> M.Map Position Int -> (Position, Int) -> [Int]
+attemptCheat maxDepth timeFromPosition cheatPoint = concatMap (computeAllSavings timeFromPosition cheatPoint) [2 .. maxDepth]
 
-toPartialCheat :: Position -> Int -> PartialCheat
-toPartialCheat position depth = PartialCheat depth neighbours
+computeAllSavings :: M.Map Position Int -> (Position, Int) -> Int -> [Int]
+computeAllSavings positionTimings (startPosition, time) depth = map (timeToBeat -) usefulPoints
   where
-    neighbours = neighboursWithDistance depth position
+    neighbours = neighboursWithDistance depth startPosition
+    timeToBeat = time - depth
+    usefulPoints = filter (< timeToBeat) $ mapMaybe (`M.lookup` positionTimings) neighbours
 
-cheatScores :: M.Map Position Int -> Int -> PartialCheat -> [Int]
-cheatScores timeFromPosition time (PartialCheat steps points) = map ((+steps) . (+time)) usefulPoints
-  where
-    usefulPoints = mapMaybe (`M.lookup` timeFromPosition) points
-
+-- Compute all points with manhatten distance exactly N from a given point
 neighboursWithDistance :: Int -> Point -> [Point]
-neighboursWithDistance dist pt = nub $ concatMap (applyOffset pt) offsets
+neighboursWithDistance dist pt = concatMap (applyOffset pt) offsets
   where
-    offsets = [0..dist] `zip` reverse [0..dist]
+    offsets = [0 .. dist - 1] `zip` reverse [1 .. dist]
 
 applyOffset :: Point -> (Int, Int) -> [Point]
-applyOffset (V2 x y) (offset, inverseOffset) = [
-  V2 (x+offset) (y+inverseOffset),
-  V2 (x+inverseOffset) (y-offset),
-  V2 (x-offset) (y-inverseOffset),
-  V2 (x-inverseOffset) (y+offset)]
+applyOffset (V2 x y) (offset, inverseOffset) =
+  [ V2 (x + offset) (y + inverseOffset),
+    V2 (x + inverseOffset) (y - offset),
+    V2 (x - offset) (y - inverseOffset),
+    V2 (x - inverseOffset) (y + offset)
+  ]
 
 computeSavings :: Int -> Grid -> [Int]
-computeSavings cheatDepth grid = Data.List.map (totalTime -) $ concatMap (filter (< totalTime) . attemptCheat grid cheatDepth timeFromPosition) cheatLocations
+computeSavings cheatDepth grid = concatMap (attemptCheat cheatDepth positionTimings) cheatLocations
   where
     raceState = race grid
     totalTime = time raceState
-    timeToPosition = M.insert (end raceState) totalTime $ visited raceState
-    timeFromPosition = M.map (totalTime -) timeToPosition
-    cheatLocations = M.toList timeToPosition
+    positionTimings = M.insert (end raceState) 0 $ M.map (totalTime -) $ visited raceState
+    cheatLocations = M.toList positionTimings
 
 part1 :: Grid -> Int
-part1 = length . filter (>=100) . computeSavings 2
+part1 = length . filter (>= 100) . computeSavings 2
 
 part2 :: Grid -> Int
-part2 = length . filter (>=100) . computeSavings 20
+part2 = length . filter (>= 100) . computeSavings 20
