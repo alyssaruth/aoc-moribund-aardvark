61d937289eaec8a1b44c94fdc88624477d6345ff
alyssa <alyssa.burlton@gmail.com>
2024-12-29 11:52:58 +0000

    naive slow 23B

--- src/Solutions/Day23.hs
+++ src/Solutions/Day23.hs
@@ -1,15 +1,21 @@
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
 {-# HLINT ignore "Use tuple-section" #-}
 module Solutions.Day23
-  ( aoc23
-  ) where
+  ( aoc23,
+  )
+where
 
-import           Common.AoCSolutions (AoCSolution (MkAoCSolution),
-                                      printSolutions, printTestSolutions)
-import           Text.Trifecta       (Parser, alphaNum, count, string, some, TokenParsing (token))
+import Common.AoCSolutions
+  ( AoCSolution (MkAoCSolution),
+    printSolutions,
+    printTestSolutions,
+  )
+import Data.List (foldl, intercalate, intersect, isPrefixOf, nub, (\\))
 import qualified Data.Map as M
-import Data.List (intersect, (\\), nub, isPrefixOf)
-import Data.Set (Set, fromList)
+import Data.Set (Set, empty, foldl, fromList, insert, intersection, size, toList, union)
+import Debug.Trace
+import Text.Trifecta (Parser, TokenParsing (token), alphaNum, count, some, string)
 
 type Computer = String
 
@@ -31,8 +37,8 @@ parseConnection = do
 makeConnectionsMap :: [(Computer, Computer)] -> M.Map Computer [Computer]
 makeConnectionsMap pairs = M.fromListWith (++) $ concatMap (\(a, b) -> [(a, [b]), (b, [a])]) pairs
 
-findTriples :: M.Map Computer [Computer] -> [Set Computer]
-findTriples network = expandTriples (M.keys network) network []
+findSubnetworks :: M.Map Computer [Computer] -> [Set Computer]
+findSubnetworks network = expandTriples (M.keys network) network []
 
 expandTriples :: [Computer] -> M.Map Computer [Computer] -> [Set Computer] -> [Set Computer]
 expandTriples computersRemaining network triplesSoFar
@@ -44,7 +50,7 @@ expandTriples computersRemaining network triplesSoFar
     newMap = purgeComputer nextComputer network
 
 purgeComputer :: Computer -> M.Map Computer [Computer] -> M.Map Computer [Computer]
-purgeComputer comp network = M.map (filter (/=comp)) $ M.delete comp network
+purgeComputer comp network = M.map (filter (/= comp)) $ M.delete comp network
 
 findTriplesForComputer :: Computer -> M.Map Computer [Computer] -> [Set Computer]
 findTriplesForComputer comp network = nub $ concatMap (findTriplesForPair network comp) connectedComps
@@ -57,10 +63,25 @@ findTriplesForPair network a b = map (\c -> fromList [a, b, c]) sharedConnection
     sharedConnections = (network M.! a) `intersect` (network M.! b)
 
 part1 :: [(Computer, Computer)] -> Int
-part1 = length . filter canContainChiefHistorian . findTriples . makeConnectionsMap
+part1 = length . filter canContainChiefHistorian . findSubnetworks . makeConnectionsMap
 
 canContainChiefHistorian :: Set Computer -> Bool
 canContainChiefHistorian = any (isPrefixOf "t")
 
+expandToLargest :: M.Map Computer [Computer] -> [Set Computer] -> Set Computer
+expandToLargest network subnetworks
+  | null expandedSubnetworks = head subnetworks
+  | otherwise = traceShow expandedSubnetworks $ expandToLargest network expandedSubnetworks
+  where
+    expandedSubnetworks = nub $ concatMap (expandSubnetwork network) subnetworks
+
+expandSubnetwork :: M.Map Computer [Computer] -> Set Computer -> [Set Computer]
+expandSubnetwork network subnetwork = map (`insert` subnetwork) intersections
+  where
+    intersections = toList $ Data.List.foldl intersection (M.keysSet network) $ map (fromList . (network M.!)) $ toList subnetwork
+
 part2 :: [(Computer, Computer)] -> String
-part2 = undefined
+part2 pairs = intercalate "," $ toList largestSubnetwork
+  where
+    network = makeConnectionsMap pairs
+    largestSubnetwork = expandToLargest network $ findSubnetworks network
