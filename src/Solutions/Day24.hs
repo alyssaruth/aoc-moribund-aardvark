250403305370d9f4611721db8f26c5ec3a9b8645
alyssa <alyssa.burlton@gmail.com>
2024-12-31 16:14:50 +0000

    close to working 24B...

--- src/Solutions/Day24.hs
+++ src/Solutions/Day24.hs
@@ -1,26 +1,31 @@
-module Solutions.Day24
-where
+module Solutions.Day24 where
 
 import Common.AoCSolutions
   ( AoCSolution (MkAoCSolution),
     printSolutions,
     printTestSolutions,
   )
+import Control.Exception (SomeException)
 import Control.Lens (imap)
 import Data.Bits (xor, (.&.), (.|.))
-import Data.List (isPrefixOf, sortBy, sortOn, (\\))
+import Data.Char (digitToInt, intToDigit, isDigit)
+import Data.Either (isRight)
+import Data.List (isPrefixOf, sortBy, sortOn, (\\), intercalate, sort)
 import qualified Data.Map as M
+import Data.Maybe (fromMaybe, isNothing, fromJust, isJust)
 import qualified Data.Ord
+import Data.Set (Set, empty, fromList, intersection, member, union)
+import Debug.Trace (traceShow)
+import Numeric (showIntAtBase)
 import Text.Parser.Char (alphaNum)
 import Text.Trifecta (Parser, integer, letter, many, some, string, token, try)
-import Numeric (showIntAtBase)
-import Data.Char (intToDigit, isDigit, digitToInt)
-import Debug.Trace (traceShow)
+
+maxDepth = 44
 
 type Wire = String
 
 data WireEquation = WireEquation {leftWire :: Wire, rightWire :: Wire, operation :: String, destinationWire :: Wire}
-  deriving (Eq)
+  deriving (Eq, Show)
 
 aoc24 :: IO ()
 aoc24 = do
@@ -53,11 +58,12 @@ parseOperator "OR" = (.|.)
 parseOperator "AND" = (.&.)
 
 part1 :: (M.Map Wire Int, [WireEquation]) -> Int
-part1 (knownWires, equations) = getBinaryNumber "z" $ resolveSystem knownWires equations
+part1 (knownWires, equations) = getBinaryNumber "z" $ fromJust $ resolveSystem knownWires equations
 
-resolveSystem :: M.Map Wire Int -> [WireEquation] -> M.Map Wire Int
+resolveSystem :: M.Map Wire Int -> [WireEquation] -> Maybe (M.Map Wire Int)
 resolveSystem knownWires wireEquations
-  | null wireEquations = knownWires
+  | null wireEquations = Just knownWires
+  | null solvableEquations = Nothing --Some sort of cycle has occurred
   | otherwise = resolveSystem newMap (wireEquations \\ solvableEquations)
   where
     solvableEquations = filter (isSolvable knownWires) wireEquations
@@ -86,30 +92,120 @@ toDecimalPart :: Int -> Int -> Int
 toDecimalPart ix value = (2 ^ ix) * value
 
 part2 :: (M.Map Wire Int, [WireEquation]) -> String
-part2 (knownWires, equations) = performAddition knownWires equations 15 23
+part2 (_, equations) = intercalate "," $ sort $ fixAdder equations []
 
--- doOneDigitAdditions :: M.Map Wire Int -> [WireEquation] -> Int
--- doOneDigitAdditions wires equations = undefined
---   where
---     pairs = sub
+possibleSwaps :: [WireEquation] -> [[WireEquation]]
+possibleSwaps equations = choose equations 2
 
+choose :: [b] -> Int -> [[b]]
+_ `choose` 0 = [[]]
+[] `choose` _ = []
+(x : xs) `choose` k = (x :) `fmap` (xs `choose` (k - 1)) ++ xs `choose` k
+
+fixAdder :: [WireEquation] -> [Wire] -> [Wire]
+fixAdder equations wiresSwapped
+  | isNothing result = wiresSwapped
+  | otherwise = traceShow ("Failed at depth " ++ show result) $ fixAdder newEquations (wiresSwapped ++ newWires)
+  where
+    result = verifyAdder maxDepth equations
+    (newEquations, newWires) = fixDepth equations (fromMaybe 0 result)
+
+fixDepth :: [WireEquation] -> Int -> ([WireEquation], [Wire])
+fixDepth equations depth
+  | depth == 0 = (equations, [])
+  | otherwise = traceShow ("Swapped wires " ++ show swappedWires) (fixedMachine, swappedWires)
+  where
+    swapsToTest = possibleSwaps $ swappableWires equations depth
+    sortedSwapsToTest = sortOn (Data.Ord.Down . (\[a, b] -> includesWires [makeWire "z" depth] a || includesWires [makeWire "z" depth] b)) swapsToTest
+    correctSwap = traceShow ("Testing " ++ show (length swapsToTest) ++ " swaps") $ head $ filter (testSwap equations depth) sortedSwapsToTest
+    swappedWires = map destinationWire correctSwap
+    fixedMachine = performSwap equations correctSwap
+
+-- If we've failed at depth N, then we know that x0...xn-1, y0...yn-1 and z0...zn-1 are wired up right already
+swappableWires :: [WireEquation] -> Int -> [WireEquation]
+swappableWires equations failedDepth = filter (not . includesWires allWires) equations
+  where
+    xWires = map (makeWire "x") [0..(failedDepth-1)]
+    yWires = map (makeWire "y") [0..(failedDepth-1)]
+    zWires = map (makeWire "z") [0..(failedDepth-1)]
+    allWires = xWires ++ yWires ++ zWires
+
+includesWires :: [Wire] -> WireEquation -> Bool
+includesWires wires (WireEquation leftWire rightWire _ destinationWire) = leftWire `elem` wires || rightWire `elem` wires || destinationWire `elem` wires
+
+testSwap :: [WireEquation] -> Int -> [WireEquation] -> Bool
+--testSwap equations depth swap = traceShow ("Tested swap " ++ show swap ++ " result = " ++ show result) result
+testSwap equations depth swap = result
+  where
+    newMachine = performSwap equations swap
+    result = verifyNoCycles newMachine && isNothing (verifyAdder depth newMachine)
+
+-- Performing arbitrary swaps can result in cycles
+verifyNoCycles :: [WireEquation] -> Bool
+verifyNoCycles machine = isJust $ performAddition machine 0 0
+
+performSwap :: [WireEquation] -> [WireEquation] -> [WireEquation]
+performSwap equations [a, b] = [newEquationA, newEquationB] ++ filter (/= b) (filter (/= a) equations)
+  where
+    newEquationA = WireEquation (leftWire a) (rightWire a) (operation a) (destinationWire b)
+    newEquationB = WireEquation (leftWire b) (rightWire b) (operation b) (destinationWire a)
+
+verifyAdder :: Int -> [WireEquation] -> Maybe Int
+verifyAdder = verifyAllDepths 0
+
+-- Verify depths incrementally until one fails
+verifyAllDepths :: Int -> Int -> [WireEquation] -> Maybe Int
+verifyAllDepths depth maxDepth equations
+  | not result = Just depth
+  | depth == maxDepth = Nothing
+  | otherwise = verifyAllDepths (depth + 1) maxDepth equations
+  where
+    result = testBinaryAdder depth equations
+
+-- Test the binary adder at a specific "depth", by throwing a set of additions at it and verifying the results
+testBinaryAdder :: Int -> [WireEquation] -> Bool
+testBinaryAdder depth equations = all (testPasses equations) tests
+  where
+    tests = prepareTestSums depth
+
+testPasses :: [WireEquation] -> (Int, Int) -> Bool
+testPasses equations (x, y) = performAddition equations x y == Just (x + y)
+
+prepareTestSums :: Int -> [(Int, Int)]
+prepareTestSums depth =
+  concatMap
+    (\(x, y) -> [(x, y), (y, x)])
+    [ (minValue, 0),
+      (minValue, maxValue - minValue),
+      (minValue `div` 2, maxValue `div` 2),
+      (minValue `div` 2, minValue `div` 2)
+    ]
+  where
+    minValue = 2 ^ depth
+    maxValue = 2 ^ (depth + 1) - 1
+
+-- Set up inputs for X and Y to perform a specific addition
+performAddition :: [WireEquation] -> Int -> Int -> Maybe Int
+-- performAddition wireEquations x y = traceShow (show x ++ " + " ++ show y ++ " = " ++ show solution) solution
+performAddition wireEquations x y
+  | isNothing resolved = Nothing
+  | otherwise = Just $ getBinaryNumber "z" (fromJust resolved)
+  where
+    xWires = prepareWires "x" $ base2 x
+    yWires = prepareWires "y" $ base2 y
+    resolved = resolveSystem (M.union xWires yWires) wireEquations
 
 base2 :: Int -> String
 base2 x = showIntAtBase 2 intToDigit x ""
 
-
-performAddition :: M.Map Wire Int -> [WireEquation] -> Int -> Int -> String
-performAddition originalWires wireEquations x y = show x ++ " + " ++ show y ++ " = " ++ show (getBinaryNumber "z" solution)
+prepareWires :: String -> String -> M.Map Wire Int
+prepareWires prefix binaryValue = M.fromList newValues
   where
-    xWires = prepareWires originalWires "x" $ base2 x
-    yWires = prepareWires originalWires "y" $ base2 y
-    solution = resolveSystem (M.union xWires yWires) wireEquations
-
-prepareWires :: M.Map Wire Int -> String -> String -> M.Map Wire Int
-prepareWires originalWires prefix binaryValue = M.fromList newValues
-  where
-    keys = map fst (wirePairs prefix originalWires)
+    keys = map (makeWire prefix) [0 .. maxDepth]
     newValues = map (\key -> (key, getNewValue binaryValue key)) keys
+
+makeWire :: String -> Int -> Wire
+makeWire prefix n = prefix ++ replicate (2 - length (show n)) '0' ++ show n
 
 getNewValue :: String -> Wire -> Int
 getNewValue binaryValue wire
